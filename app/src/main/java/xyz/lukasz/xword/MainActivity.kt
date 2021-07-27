2b29e94111fd709d74443a2110e9f8064f4250bc
Frixuu <kontakt@lukasz.xyz>
2021-07-27 18:06:13 +0200

    Put the search box on the top app bar

--- app/src/main/java/xyz/lukasz/xword/MainActivity.kt
+++ app/src/main/java/xyz/lukasz/xword/MainActivity.kt
@@ -2,10 +2,28 @@ package xyz.lukasz.xword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // Handle tab select
+                Log.i("MA", "tab selected: ${tab?.text}")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Log.i("MA", "tab reselected: ${tab?.text}")
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Log.i("MA", "tab unselected: ${tab?.text}")
+            }
+        })
     }
 }
