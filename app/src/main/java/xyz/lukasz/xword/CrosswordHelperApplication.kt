a56e41a33dc9ff53035cab4b52b46c323875e7cf
Frixuu <kontakt@lukasz.xyz>
2022-03-29 02:47:34 +0200

    Extend application class

---
+++ app/src/main/java/xyz/lukasz/xword/CrosswordHelperApplication.kt
@@ -0,0 +1,10 @@
+package xyz.lukasz.xword
+
+import android.app.Application
+
+class CrosswordHelperApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
